f6dccc03a95ecebbe1823db08cf4691555308a9e
Sara Kerri <sara.kerri@elev.ga.ntig.se>
2025-02-10 14:57:44 +0100

    tränat på event och name

---
+++ ÄndraBakgrundsfärg/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ÄndraBakgrundsfärg;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ClickCyan(object sender, RoutedEventArgs e)
+    {
+        // Ändra färg till cyan
+        this.Background = Brushes.Cyan;
+    }
+
+     private void ClickLavender(object sender, RoutedEventArgs e)
+    {
+        // Ändra färg till Lavender
+        this.Background = Brushes.Lavender;
+    }
+}
